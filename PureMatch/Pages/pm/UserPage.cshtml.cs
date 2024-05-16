9702ef01ebe504aefb14bc4d9f076e8dca6cc015
= <nathaniel.risum2@gmail.com>
2024-05-16 20:12:55 +0200

    Add session to userpage

--- PureMatch/Pages/pm/UserPage.cshtml.cs
+++ PureMatch/Pages/pm/UserPage.cshtml.cs
@@ -6,28 +6,23 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PureMatch.Pages.pm
 {
-    
+    [BindProperties]
     public class UserPageModel : PageModel
     {
         private readonly DataBaseLink _repo;
-        private User _ouser;
-        private User _matchuser; 
+        private User? _matchuser; 
 
         public UserPageModel(DataBaseLink repo) { 
-            _repo = repo;
+            _repo = repo; 
         }
-        [BindProperty]
-        public User OUser { get { return _ouser; } set { _ouser = value;} }
-        [BindProperty]
-        public User MatchUser { get { return _matchuser; } set { _matchuser = value; } }
-        [BindProperty]
+       
+        public User MatchUser { get { return _matchuser!; } set { _matchuser = value; } }
         [Required (ErrorMessage = "Du skal skrive en besked!")]
         [StringLength(100, MinimumLength = 1, ErrorMessage = "Besked skal være mindst 2 tegn lang")]
         public string Message { get; set; }
 
-        public void OnGet(int userid ,int mid)
+        public void OnGet(int mid)
         {
-            _ouser = _repo.ReadUser(2); 
             _matchuser = _repo.ReadUser(mid);
             _matchuser.Days = _repo.ReadDays(mid);
             _matchuser.MuscleGroups = _repo.ReadMuscleGroups(mid); 
@@ -37,9 +32,6 @@ namespace PureMatch.Pages.pm
 
         public IActionResult OnPost(int ownid, int matchid)
         {
-            OUser = _repo.ReadUser(ownid);
-            OUser.Days = _repo.ReadDays(ownid);
-            OUser.MuscleGroups = _repo.ReadMuscleGroups(ownid); 
             MatchUser = _repo.ReadUser(matchid);
             MatchUser.Days = _repo.ReadDays(matchid);
             MatchUser.MuscleGroups = _repo.ReadMuscleGroups(matchid);
@@ -51,7 +43,7 @@ namespace PureMatch.Pages.pm
             }
              
             _repo.SendMessage(ownid, matchid, Message);
-            return RedirectToPage("./PureMatch", new { userid = ownid}); 
+            return RedirectToPage("./PureMatch"); 
         }
 
 
