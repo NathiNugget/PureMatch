49ad39b9b0144e36416202082958de475369b2fd
= <nathaniel.risum2@gmail.com>
2024-05-27 14:50:56 +0200

    Ignore

---
+++ PureMatch/Secret.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureLib.Services
+{
+    public static class Secret
+    {
+        public static string ConnectionString = "Data Source=mssql3.unoeuro.com;Initial Catalog=nathanielrisum_dk_db_dev;User ID=nathanielrisum_dk;Password=HknxBh2wDertbf93azm4;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
