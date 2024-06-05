415d1de06bca94464707882fda868eedc611a436
Nathaniel Risum <nathaniel.risum2@gmail.com>
2024-06-05 17:41:16 +0200

    Add more information to profile creation and update Add lots of tooltips for information

--- PureMatch/wwwroot/js/site.js
+++ PureMatch/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function myFunction() {
+    var passwordField = document.getElementById("inputField");
+    var passicon = document.getElementById("eyeIcon"); 
+
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        document.getElementById("eyeIcon").src = "/images/eye_hide.png"
+        document.getElementById("passmessage").innerText = "Skjul adgangskode";
+        
+    }
+    else {
+        passwordField.type = "password";
+        document.getElementById("eyeIcon").src = "/images/eye.png"
+        document.getElementById("passmessage").innerText = "Vis adgangskode";
+
+
+    }
+
+  
+}
